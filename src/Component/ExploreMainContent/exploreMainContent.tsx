1eca25ed92ec203751603e8ce75396de1b04645a
Dragonek <dragoneknp@gmail.com>
2021-12-29 23:08:43 +0300

    fix: fixed some issues

--- src/Component/ExploreMainContent/exploreMainContent.tsx
+++ src/Component/ExploreMainContent/exploreMainContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { createRef, useEffect, useRef, useState } from "react";
 import "./exploreMainContent.scss";
 import MakeExploreMainContentCards from "../ExploreMainContentCards/ExploreMainContentCards";
 import Advertisment from "../Advertisment/advertisment";
@@ -13,14 +13,45 @@ interface CarCard {
     id: string | undefined;
 }
 const ExploreMainContent = (props: any) => {
-    const data = props.data;
+    const headerRef = useRef<HTMLDivElement>(null);
+    let data: CarCard[] = props.data;
+    const [newData, changeNewData] = useState(props.data);
+
     const [currentPage, changeCurrentPage] = useState(
         localStorage.getItem("explorePage")
             ? Number(localStorage.getItem("explorePage"))
             : 1
     );
+    const scrollToHeader = () => {
+        setTimeout(() => {
+            window.scrollTo(
+                0,
+
+                Number(headerRef.current?.getBoundingClientRect().top) +
+                    window.pageYOffset -
+                    100
+            );
+        }, 100);
+    };
+    const [inputValue, changeInputValue] = useState("");
+    const [isSubmitted, changeIsSubmitted] = useState(false);
+
+    useEffect(() => {
+        changeNewData(
+            data.filter(
+                (item) =>
+                    item.brand?.startsWith(inputValue) ||
+                    item.model?.startsWith(inputValue)
+            )
+        );
+    }, [isSubmitted, data]);
+
     const makeArrayWithPagination = (numberOfPages: number) => {
-        return Array((numberOfPages % 5) + 1)
+        return Array(
+            Math.round(numberOfPages / 5) > 0
+                ? Math.round(numberOfPages / 5)
+                : 1
+        )
             .fill(0)
             .map((item, index) => index + 1);
     };
@@ -30,22 +61,32 @@ const ExploreMainContent = (props: any) => {
             <div className="exploreMainContent__container container">
                 <div className="exploreMainContent__search">
                     <input
+                        value={inputValue}
+                        onChange={(event) =>
+                            changeInputValue(event.target.value)
+                        }
                         type="text"
                         className="exploreMainContent__input"
                         placeholder="Search smth.."
                     />
-                    <button className="exploreMainContent__button redBtn">
+                    <button
+                        onClick={() => changeIsSubmitted(!isSubmitted)}
+                        className="exploreMainContent__button redBtn"
+                    >
                         Search
                     </button>
                 </div>
-                <div className="exploreMainContent__header">Founded Model</div>
+                <div className="exploreMainContent__header" ref={headerRef}>
+                    Founded Model
+                </div>
                 <div className="exploreMainContent__body exploreMainContent-body">
                     <div className="exploreMainContent-body__content">
                         <div className="exploreMainContent-body__resultsHeader">
-                            We’ve found 20 results of tesla
+                            We’ve found {newData.length} results
                         </div>
                         <div className="exploreMainContent-body__results">
-                            {MakeExploreMainContentCards(data, currentPage)}
+                            {/* {MakeExploreMainContentCards(data, currentPage)} */}
+                            {MakeExploreMainContentCards(newData, currentPage)}
                         </div>
                         <div className="exploreMainContent__pagination">
                             <img
@@ -59,10 +100,15 @@ const ExploreMainContent = (props: any) => {
                                 onClick={() => {
                                     if (currentPage > 1) {
                                         changeCurrentPage(currentPage - 1);
+                                        scrollToHeader();
+                                        localStorage.setItem(
+                                            "explorePage",
+                                            (currentPage - 1).toString()
+                                        );
                                     }
                                 }}
                             />
-                            {makeArrayWithPagination(data.length).map(
+                            {makeArrayWithPagination(newData.length).map(
                                 (page) => (
                                     <div
                                         className={`exploreMainContent__page ${
@@ -72,6 +118,7 @@ const ExploreMainContent = (props: any) => {
                                         }`}
                                         onClick={() => {
                                             changeCurrentPage(page);
+                                            scrollToHeader();
                                             localStorage.setItem(
                                                 "explorePage",
                                                 page.toString()
@@ -85,15 +132,23 @@ const ExploreMainContent = (props: any) => {
                             )}
                             <img
                                 src={`${
-                                    currentPage < (data.length % 5) + 1
+                                    currentPage < Math.round(newData.length / 5)
                                         ? "/images/arrowLeftActive.svg"
                                         : "/images/arrowLeft.svg"
                                 }`}
                                 alt="Next Page"
                                 className="arrowRight"
                                 onClick={() => {
-                                    if (currentPage < (data.length % 5) + 1) {
+                                    if (
+                                        currentPage <
+                                        Math.round(newData.length / 5)
+                                    ) {
                                         changeCurrentPage(currentPage + 1);
+                                        scrollToHeader();
+                                        localStorage.setItem(
+                                            "explorePage",
+                                            (currentPage + 1).toString()
+                                        );
                                     }
                                 }}
                             />
--- src/Component/ExploreMainContentCards/ExploreMainContentCards.tsx
+++ src/Component/ExploreMainContentCards/ExploreMainContentCards.tsx
@@ -4,7 +4,7 @@ import ExploreMainContentCard from "../ExploreMainContentCard/exploreMainContent
 const MakeExploreMainContentCards = (data: any[], currentPage: number) => {
     return (
         <div key={currentPage} className="exploreMainContent-body__carCards">
-            {data.slice(currentPage - 1, currentPage + 4).map((card) => (
+            {data.slice((currentPage - 1) * 5, currentPage * 5).map((card) => (
                 <ExploreMainContentCard
                     {...card}
                     key={`${card.brand}_${card.model}_${card.HP}_${card.typeOfFuel}_${card.transmission}_${card.price}`}
